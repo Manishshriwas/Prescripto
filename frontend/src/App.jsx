6c2cab4997ff65b3d0706108c0c022a0561c2b82
Manish shriwas <133646318+Manishshriwas@users.noreply.github.com>
2026-04-22 13:14:13 +0530

    Import Chatbot component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import Chatbot from "./pages/Chatbot";
 
 
 
